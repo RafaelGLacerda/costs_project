48d7998a57282b33f4997602232a59e9b3e39224
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2024-12-03 17:30:50 -0300

    Update Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,7 +2,6 @@ import styles from './Project.module.css'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { parse, v4 as uuidv4 } from 'uuid'
-import { parse } from 'alguma-biblioteca';
 
 
 import Loading from '../layout/Loading'
